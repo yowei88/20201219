063cdc63703ea67ed170d8be023158bb21770a83
yowei88 <73010491+yowei88@users.noreply.github.com>
2020-12-19 12:09:38 +0800

    Add files via upload

---
+++ 1219-3.py
@@ -0,0 +1,12 @@
+def tree(x,y,z):
+    mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,161)
+    mc.setBlocks(x,y,z,x,y+4,z,17)
+    
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+x,y,z=mc.player.getTilePos()  
+for a  in range (1,1001):
+    for i in range (1,1001):
+        tree(x+a*5,y+i*7,z)
+    
